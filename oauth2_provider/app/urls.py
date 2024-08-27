67f46cb87e155386c5021a1ce68686011f3200db
wang-guangge <wangguangge@huawei.com>
2024-08-27 19:32:30 +0800

    add account manage operations

---
+++ oauth2_provider/app/urls.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+# ******************************************************************************
+# Copyright (c) Huawei Technologies Co., Ltd. 2021-2021. All rights reserved.
+# licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN 'AS IS' BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# ******************************************************************************/
+
+from oauth2_provider.app.views.account import AddUser, ChangePassword, Login, Logout, ManagerLogin
+from oauth2_provider.app.views.applications import ApplicationsDetailView, ApplicationsRegisteView, ApplicationsView
+from oauth2_provider.app.views.oauth2 import (
+    OauthIntrospectView,
+    OauthorizeView,
+    OauthRevokeView,
+    OauthTokenView,
+    RefreshTokenView,
+)
+
+URLS = [
+    # applications
+    (ApplicationsView, "/oauth2/applications"),
+    (ApplicationsRegisteView, "/oauth2/applications/register"),
+    (ApplicationsDetailView, "/oauth2/applications/<string:client_id>"),
+    # oauth2
+    (OauthorizeView, "/oauth2/authorize"),
+    (OauthTokenView, "/oauth2/token"),
+    (OauthRevokeView, "/oauth2/revoke-token"),
+    (OauthIntrospectView, "/oauth2/introspect"),
+    (RefreshTokenView, "/oauth2/refresh-token"),
+    # account
+    (AddUser, "/oauth2/register"),
+    (ManagerLogin, "/oauth2/manager-login"),
+    (Login, "/oauth2/login"),
+    (Logout, "/oauth2/logout"),
+    (ChangePassword, "/oauth2/password"),
+]
---
+++ oauth2_provider/app/views/account.py
@@ -0,0 +1,147 @@
+#!/usr/bin/python3
+# ******************************************************************************
+# Copyright (c) Huawei Technologies Co., Ltd. 2021-2024. All rights reserved.
+# licensed under the Mulan PSL v2.
+# You can use this software according to the terms and conditions of the Mulan PSL v2.
+# You may obtain a copy of Mulan PSL v2 at:
+#     http://license.coscl.org.cn/MulanPSL2
+# THIS SOFTWARE IS PROVIDED ON AN 'AS IS' BASIS, WITHOUT WARRANTIES OF ANY KIND, EITHER EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT, MERCHANTABILITY OR FIT FOR A PARTICULAR
+# PURPOSE.
+# See the Mulan PSL v2 for more details.
+# ******************************************************************************/
+from flask import g, make_response
+from oauth2_provider.app import cache
+from oauth2_provider.app.core.account import UserProxy
+from oauth2_provider.app.serialize.account import AddUserSchema, LoginSchema, LogoutSchema, ResetPasswordSchema
+from oauth2_provider.app.views import login_require, validate_request
+from vulcanus.restful.resp import state
+from vulcanus.restful.response import BaseResponse
+
+
+class AddUser(BaseResponse):
+    """
+    Interface for register user.
+    Restful API: post
+    """
+
+    @validate_request(schema=AddUserSchema)
+    def post(self, request_body, *args, **kwargs):
+        """
+        Add user
+
+        Args:
+            username (str)
+            password (str)
+            email (str)
+
+        Returns:
+            dict: response body
+        """
+        register_res = UserProxy().register_user(request_body)
+        if register_res != state.SUCCEED:
+            return self.response(code=register_res, message="register user failed.")
+        return self.response(code=state.SUCCEED)
+
+
+class Login(BaseResponse):
+    """
+    Interface for user login.
+    Restful API: post
+    """
+
+    @validate_request(schema=LoginSchema)
+    def post(self, request_body, *args, **kwargs):
+        """
+        User login
+
+        Args:
+            username (str)
+            password (str)
+
+        Returns:
+            dict: response body
+        """
+        status_code, user_token = UserProxy().login(request_body)
+        if status_code != state.SUCCEED:
+            return self.response(code=status_code)
+        response = make_response(self.response(code=status_code, data=dict(user_token=user_token)))
+        if not request_body["for_validate"]:
+            # 30 days expire
+            response.set_cookie("Authorization", user_token, 60 * 60 * 24 * 30)
+            cache.set(request_body["username"] + "-token", user_token)
+            cache.expire(request_body["username"] + "-token", 60 * 60 * 24 * 30)
+        return response
+
+
+class ManagerLogin(BaseResponse):
+    """
+    Interface for user login.
+    Restful API: post
+    """
+
+    @validate_request(schema=LoginSchema)
+    def post(self, request_body, *args, **kwargs):
+        """
+        User login
+
+        Args:
+            username (str)
+            password (str)
+
+        Returns:
+            dict: response body
+        """
+        status_code, user_token = UserProxy().manager_login(request_body)
+        if status_code != state.SUCCEED:
+            return self.response(code=status_code)
+        user_token = "bearer " + user_token
+        response = make_response(self.response(code=status_code, data=dict(user_token=user_token)))
+        response.set_cookie('Authorization', '', expires=0)
+        cache.set(request_body["username"] + "-manager-token", user_token)
+        cache.expire(request_body["username"] + "-manager-token", 60 * 60 * 2)
+        return response
+
+
+class ChangePassword(BaseResponse):
+    """
+    Interface for user change password.
+    Restful API: post
+    """
+
+    @login_require
+    @validate_request(schema=ResetPasswordSchema)
+    def post(self, request_body, *args, **kwargs):
+        reset_res = UserProxy().reset_password(request_body)
+        return self.response(code=reset_res)
+
+
+class Logout(BaseResponse):
+    """
+    Interface for logout.
+    Restful API: post
+    """
+
+    @login_require
+    @validate_request(schema=LogoutSchema)
+    def post(self, request_body, *args, **kwargs):
+        """
+        Logout.
+
+        Args:
+            token(str): access token of client,
+            client_id(str): client id
+
+        Returns:
+            dict: response body
+        """
+        logout_res, username = UserProxy().logout(request_body)
+        if logout_res != state.SUCCEED:
+            return self.response(code=logout_res)
+        if g.is_manage_user:
+            cache.delete(username + "-manager-token")
+        else:
+            cache.delete(username + "-token")
+
+        response = make_response(self.response(code=state.SUCCEED))
+        return response

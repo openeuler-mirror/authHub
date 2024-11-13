c62c9bb20aa024732692cedabc40f417dda814d4
gongzt <gong_zhengtang@163.com>
2024-11-13 14:51:11 +0800

    修复由https://gitee.com/openeuler/authHub/pulls/17引发的NoneType错误

--- oauth2_provider/app/core/server.py
+++ oauth2_provider/app/core/server.py
@@ -82,7 +82,7 @@ class AuthorizationServer(AuthlibAuthorizationServer):
             raise InvalidScopeError(state=state)
 
     def create_oauth2_request(self, request):
-        return OAuth2Request(request)
+        return OAuth2Request(flask_req)
 
     def create_bearer_token_generator(self, config):
         """Create a generator function for generating ``token`` value. This

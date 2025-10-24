e355b6aab0357a25a93c63622cdf1717d9496023
Ethan-Zhang <ethanzhang55@outlook.com>
2025-10-24 21:59:39 +0800

    Feat: 新增login页窗体大小适配

--- oauth2_web/src/layouts/UserLayout.vue
+++ oauth2_web/src/layouts/UserLayout.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
 import { ElRow, ElCol } from 'element-plus'
+import { ref, onMounted, onUnmounted } from 'vue'
+
+const windowWidth = ref(window.innerWidth)
+
+const updateWindowWidth = () => {
+  windowWidth.value = window.innerWidth
+}
+
+onMounted(() => {
+  window.addEventListener('resize', updateWindowWidth)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWindowWidth)
+})
 </script>
 <template>
   <el-row class="user-layout-wrapper">
-    <el-col class="left-side" :md="{ span: 24 }" :lg="{ span: 12 }" :xl="{ span: 12 }">
+    <el-col class="left-side" :lg="{ span: 12 }" :xl="{ span: 12 }" v-show="windowWidth >= 2400">
       <img src="@/assets/imgs/loginPage.png" alt="loginPage" />
     </el-col>
-    <el-col class="right-side" :sm="{ span: 24 }" :lg="{ span: 12 }" :xl="{ span: 12 }">
+    <el-col class="right-side" :xs="{ span: 24 }" :sm="{ span: 24 }" :md="{ span: 24 }" :lg="{ span: 12 }" :xl="{ span: 12 }">
       <header class="header">
         <a href="/">
           <div class="logo">
@@ -34,6 +49,13 @@ import { ElRow, ElCol } from 'element-plus'
     margin: auto;
     top: 50vh;
     margin-top: -150px;
+  }
+}
+
+/* 媒体查询：当屏幕宽度小于992px时隐藏左侧图片 */
+@media (max-width: 2400px) {
+  .left-side {
+    display: none !important;
   }
 }
 
